15e06d30c06976fe9480cbbc263ef9e514f0947d
Alejandro Valencia <alejandrov.net@gmail.com>
2024-04-07 23:45:10 -0500

    Se validan modelo programs participants

--- app/Models/ProgramParticipant.php
+++ app/Models/ProgramParticipant.php
@@ -23,10 +23,24 @@ class ProgramParticipant extends Model
 
     public static function rulesEntity($en = null)
     {
+        $table = '';
+        switch ($en) {
+            case 'user':
+                $table = 'users';
+                break;
+            case 'company':
+                $table = 'companies';
+                break;
+            case 'challenge':
+                $table = 'challenges';
+                break;
+                default:
+                $table = $en;
+        }
         $rules = [
             'program_id' => 'required|exists:programs,id',
-            'entity_type' => 'required|string|in:users,challenges,companies',
-            'entity_id' => 'required|exists:' . $en . ',id'
+            'entity_type' => 'required|string|in:user,challenge,company',
+            'entity_id' => 'required|exists:' . $table . ',id'
         ];
         return $rules;
     }
